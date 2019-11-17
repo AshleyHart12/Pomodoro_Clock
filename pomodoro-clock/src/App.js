3cea7de748ccb2767477766ed6e8ef9f6b48bd3c
Ashinator <Ahart425@gmail.com>
2019-11-16 18:16:10 -0700

    added styling, more functionality

--- pomodoro-clock/src/App.js
+++ pomodoro-clock/src/App.js
@@ -1,125 +1,172 @@
 import React from 'react';
-
 import './App.css';
 
+const sound ='';
+
 class App extends React.Component {
-    state = {
-      breakLength: '5',
-      sessionLength: '25',
-      session: '25:00',
-      timerTime: 0,
+  constructor(props){
+    super(props)
+    this.state = {
+      breakLength: 5,
+      sessionLength: 25,
       timerOn: false,
-      seconds: 1800000 / 1000
-    }
-  
-
-  resetClock(){
-    this.setState({
-      breakLength: '5',
-      sessionLength: '25',
-      session: '25:00',
-    })
+      minutes: 25,
+      seconds: 0,
+      intervalId: 0
+    };
+    // this.handleClick = this.handleClick.bind(this);
   }
-
-  decrementBreak = () => {
+  
+decrementBreak(){
+  if (this.state.breakLength > 1){
     this.setState({
       breakLength: this.state.breakLength - 1
     })
-  }
+  }   
+};
 
-  incrementBreak = () => {
+incrementBreak(){
+  if (this.state.breakLength < 60) {
     this.setState({
-      breakLength: this.state.breakLength + 1
+    breakLength: this.state.breakLength + 1
     })
   }
+};
 
-  decrementSession = () => {
-    this.setState({
-      sessionLength: this.state.sessionLength - 1
-    })
+decrementSession(){
+  if (this.state.sessionLength > 1) {
+    this.setState(({minutes, sessionLength}) => ({
+      minutes: minutes - 1,
+      sessionLength: sessionLength - 1
+    }))
   }
+};
 
-  incrementSession = () => {
-    this.setState({
-      sessionLength: this.state.sessionLength + 1
-    })
+incrementSession(){
+  if (this.state.sessionLength < 60) {
+    this.setState(({minutes, sessionLength}) => ({
+      minutes: minutes + 1,
+      sessionLength: sessionLength + 1
+    }))
   }
+};
 
-  getSeconds() {
-    return ("0" + Math.floor(this.state.seconds % 60)).slice(-2);
-  }
+getSeconds(){
+ if (this.state.seconds > 0) {
+  return ("0" + Math.floor(this.state.seconds % 60)).slice(-2);
+ }
+};
 
-  getMinutes() {
+getMinutes() {
+  if(this.state.seconds > 0) {
     return ("0" + Math.floor(this.state.seconds % 3600 / 60)).slice(-2)
   }
+};
 
-  startCounter() {
+playAudio(){
+  sound = new sound("http://mattersofgrey.com/audio/Minecraft-meow2.mp3")  
+}
+
+handleStart(){
     this.timer = setInterval(() => {
-      this.setState({
-      seconds: this.state.seconds -1            
-        });
-    }, 1000);  
-  };
+      const { seconds, minutes } = this.state
 
-  StopCounter(){
+      if (seconds > 0) {
+        this.setState(({seconds}) => ({
+          seconds: seconds - 1
+        }))
+      }
+      if (seconds === 0) {
+        if (minutes === 0){   
+          clearInterval(this.myInterval);
+          this.playAudio()
+        } else {
+          this.setState(({minutes}) => ({
+            minutes: minutes - 1,
+            seconds: 59
+          }))
+        }
+      }
+    }, 1000)
+    };
+
+  handleStop(){
     clearInterval(this.timer);
+    }
+
+resetCounter(){
+  clearInterval(this.timer);
     this.setState({
-      timerOn: false
-    });
-  };
+      minutes: 25,
+      seconds: 0,
+      timerOn: false,
+      breakLength: 5,
+      sessionLength: 25,
+      intervalId: 0,
+      buttonText: "Start"
+    })
+};
 
-  resetCounter(){
-      this.setState({
-        timerTime: this.state.session
-      })
-  };
 
+render() {
+const { minutes, seconds } = this.state
 
-  render() {
-  return (
-    <div className="App">
+return (
+  <div className="App">
 
-      <div id="title">
-      <h1>Pomodoro Clock</h1>
+ 
+    <div id="titleHeader">
+      <p id="rainbowHeader">Pomodoro Clock
+        </p>
       </div>
 
-      <div id="labels-container">
-      <div id="break-label">
-      <h5>Break Length</h5>
-        <div id="break-label-elements">
-        <p id="break-decrement pointer" onClick={this.decrementBreak}>-</p>
-        <p id="break-number">{this.state.breakLength}</p>
-        <p id="break-increment pointer" onClick={this.incrementBreak}>+</p>
+
+  {/* BREAK AND SESSION LENGTHS + BUTTONS */}
+    <div id="labelsContainer">
+    <div id="labelHeader">
+    <label id="break-label">Break Length</label>
+      <div>
+      <button id="break-decrement" onClick={() => this.decrementBreak()}>-</button>
+      <span id="break-length">{this.state.breakLength}</span>
+      <button id="break-increment" onClick={() => this.incrementBreak()}>+</button>
+      </div>
+      </div>
+
+    <div id="labelHeader">
+    <label id="session-label">Session Length</label>
+      <div>
+      <button id="session-decrement" onClick={() => this.decrementSession()}>-</button>
+      <span id="session-length">{this.state.sessionLength}</span>
+      <button id="session-increment" onClick={() => this.incrementSession()}>+</button>
         </div>
       </div>
+    </div>    
 
-      <div id="session-label">
-      <h5>Session Length</h5>
-        <div id="session-label-elements">
-        <p id="session-decrement pointer" onClick={this.decrementSession}>-</p>
-        <p id="session-number">{this.state.sessionLength}</p>
-        <p id="session-increment pointer" onClick={this.incrementSession}>+</p>
-        </div>
+    {/* MAIN TIMER DISPLAY */}
+    <div id="timer">
+      <div id="timerContent" >
+        <label id="timer-label">Session</label>
+        <span id="time-left">{minutes} : {seconds < 10 ? `0${seconds}` : seconds}</span>
       </div>
-      </div>
-
-      <div id="timer-label">
-        <h4>Session</h4>
-        <div id="time-left">
-          {this.getMinutes()} : {this.getSeconds()}
-        </div>
-      </div>
-
-      <div id="controls">
-        <button onClick={() => this.startCounter()}>Start</button>
-        <button onClick={() => this.StopCounter()}>Stop</button>
-        <button onClick={() => this.resetCounter()}>Reset</button>
-      </div>
-
     </div>
-  );
-  }
-  
+
+    
+
+  {/* START/STOP/RESET BUTTONS */}
+    <div id="controls">     
+     
+      <button id="start_stop" onClick={()=> this.handleStart(console.log("clicked"))}>Start</button>
+      <button id="start_stop" onClick={()=> this.handleStop(console.log("clicked"))}>Stop</button>
+ 
+      <button id="reset" onClick={() => this.resetCounter()}>Reset</button>
+    </div>
+
+  </div>
+);
+}
+
 }
 
 export default App;
+
+
